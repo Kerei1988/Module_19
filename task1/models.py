8a16b3c89be6e43bb79001f93f5a475cd12466be
Kerei1988 <rustam.itchanov@gmail.com>
2024-08-10 18:32:43 +0600

    Домашнее задание по теме "Модели баз данных в Django."

---
+++ task1/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+
+# Create your models here.
+class Bayer(models.Model):
+    name = models.CharField(max_length=20, name='username')
+    balance = models.DecimalField(max_digits=20, decimal_places=2)
+    age = models.IntegerField()
+
+
+class Game(models.Model):
+    title = models.CharField(max_length=100)
+    cost = models.DecimalField(max_digits=8, decimal_places=2)
+    size = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField()
+    bayer = models.ManyToManyField(Bayer, related_name='owner')
+    age_limited = models.BooleanField(default=False)
+
+
