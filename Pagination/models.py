3bd62316076f0da7b9813a1cc464739e32552042
Kerei1988 <rustam.itchanov@gmail.com>
2024-09-24 19:09:56 +0600

    Домашнее задание по теме "Настраиваем СУБД postgre в django"

--- Pagination/models.py
+++ Pagination/models.py
@@ -11,6 +11,21 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
+class Categories(models.Model):
+    category_id = models.AutoField(primary_key=True)
+    category_name = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'Categories'
 
 
+class Goods(models.Model):
+    product_id = models.AutoField(primary_key=True)
+    product_name = models.CharField(max_length=100)
+    category = models.ForeignKey(Categories, models.DO_NOTHING, db_column='category')
+    price = models.DecimalField(max_digits=18, decimal_places=2, blank=True, null=True)
 
+    class Meta:
+        managed = False
+        db_table = 'Goods'
--- django19/settings.py
+++ django19/settings.py
@@ -76,12 +76,15 @@ WSGI_APPLICATION = 'django19.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
+'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'Urban',
+            'USER': 'postgres',
+            'PASSWORD': '123456789',
+            'HOST': '127.0.0.1',
+            'PORT': '5432',
+            }
+            }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
